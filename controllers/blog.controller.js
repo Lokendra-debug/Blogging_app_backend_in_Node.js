631ccd20259953367f964cd46dac2a625f1dca70
lokendra <thelokendrasinghrathore@gmail.com>
2023-05-20 23:52:43 +0530

    nith

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -12,20 +12,51 @@ const blogAdd=async(req,res)=>{
 }
 
 const blogGet=async(req,res)=>{
+    const user=req.body.authorID;
+    try {
+        const data=await BlogModel.find({authorID:user})
+        console.log(data)
+        return res.status(200).send(data)
+
+    } catch (error) {
+        res.status(400).send({error:error.message});
+    }
     
 }
 
 const blogGetOne=async(req,res)=>{
+    const todoID=req.params.todoID
+    try {
+        const data=await BlogModel.findById({_id:todoID})
 
+        return res.status(200).send(data)
+
+    } catch (error) {
+        res.status(400).send({error:error.message});
+    }
 }
 
 
 const blogUpdate=async(req,res)=>{
+    const todoID=req.params.todoID
+    const payload=req.body;
+    try {
+        const data=await BlogModel.findByIdAndUpdate({_id:todoID},payload)
+        return res.status(200).send(data)
+    } catch (error) {
+        res.status(400).send({error:error.message});
+    }
 
 }
 
 const blogDelete=async(req,res)=>{
-
+    const todoID=req.params.todoID
+    try {
+        const data=await BlogModel.findByIdAndDelete({_id:todoID})
+        return res.status(200).send(`User with user id ${todoID} has been deleted from the database`)
+    } catch (error) {
+        res.status(400).send({error:error.message});
+    }
 }
 
 const blogGetAll=async(req,res)=>{
